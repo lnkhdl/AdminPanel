d8915b9aac6bb41577cb6fabf86476fc1ac751b6
Lnk Hdl <lnnkhdl@gmail.com>
2021-05-18 19:47:27 +0200

    Last login at timestamp added to Users

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,10 +6,11 @@
     </div>
     
     <div class="mt-6">
-        <div class="w-1/5 px-5 py-3 bg-white shadow-lg rounded-xl">
+        <div class="w-1/4 px-5 py-3 bg-white shadow-lg rounded-xl">
             <div class="py-2 text-2xl">{{ $user->first_name }} {{ $user->last_name }}</div>
-            <div>{{ $user->email }}</div>
-            <div>{{ $user->roles[0]->name }}</div>
+            <div>Email: {{ $user->email }}</div>
+            <div>Role: {{ $user->roles[0]->name }}</div>
+            <div>Last login: {{ $user->last_login_at ?: 'not login yet'}}</div>
         </div>
     </div>
 @endsection
